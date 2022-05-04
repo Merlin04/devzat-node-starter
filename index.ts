93a6f02c74fc88b35139ff64517c35514a9b8619
merlin04 <Merlin04@users.noreply.github.com>
2022-05-03 21:41:50 -0700

    Add chalk (thanks Ishan!)

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Devzat from "devzat";
 
 // Some utilities that may be helpful (feel free to remove them)
 import fetch from "node-fetch";
+import chalk from "chalk";
 import "ts-replace-all";
 
 if(!process.env.DEVZAT_TOKEN) throw new Error("DEVZAT_TOKEN environment variable is not defined");
@@ -10,7 +11,7 @@ if(!process.env.DEVZAT_ADDRESS) throw new Error("DEVZAT_ADDRESS environment vari
 const plugin = new Devzat({
     address: process.env.DEVZAT_ADDRESS,
     token: process.env.DEVZAT_TOKEN,
-    name: "Devzat Node Starter"
+    name: chalk.red("Devzat Node Starter")
 });
 
 // Now, you can use the plugin instance
